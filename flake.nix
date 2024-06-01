7fcae9e4664e2e86d561b7301476e6fbc681d1b0
zimbatm <zimbatm@zimbatm.com>
2024-06-01 14:17:40 +0200

    flake: update terraform

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   # Add all your dependencies here
   inputs = {
     blueprint.url = "github:numtide/blueprint";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
   # Keep the magic invocations to minimum.
